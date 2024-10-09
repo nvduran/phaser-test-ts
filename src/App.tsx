75e45c3c15a2c03813eb9a04126eb99ecd719c1c
nvduran <nvduran@gmail.com>
2024-10-09 01:15:36 -0500

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import BossGame from './components/BossGameComponent.tsx'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
